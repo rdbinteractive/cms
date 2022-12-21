9ed848b22a654f7a06be34491c38847ace667e5b
Robert Bardall <robert.bardall@rdbinteractive.com>
2022-12-21 16:11:14 -0500

    Publication Type Resource

--- src/FilamentCmsServiceProvider.php
+++ src/FilamentCmsServiceProvider.php
@@ -5,11 +5,16 @@ namespace RDBI\CMS;
 use Filament\PluginServiceProvider;
 use RDBI\CMS\Filament\Resources\PublicationResource;
 use RDBI\CMS\Filament\Resources\PublicationTypeResource;
+use RDBI\CMS\Filament\Resources\PublicationTypeResource\RelationManagers\PublicationsRelationManager;
 use Spatie\LaravelPackageTools\Package;
 
 class FilamentCmsServiceProvider extends PluginServiceProvider
 {
     public static string $name = 'laravel-cms';
+
+    protected array $relationManagers = [
+        PublicationsRelationManager::class
+    ];
 
     protected array $resources = [
         PublicationResource::class,
