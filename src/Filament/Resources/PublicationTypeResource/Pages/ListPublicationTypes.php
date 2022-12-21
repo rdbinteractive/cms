427b05f4ae1c5934c8d21be1352f3a20c8af1b47
Robert Bardall <robert.bardall@rdbinteractive.com>
2022-12-21 15:34:06 -0500

    Publication Type Resource

--- src/Filament/Resources/PublicationTypeResource/Pages/ListPublicationTypes.php
+++ src/Filament/Resources/PublicationTypeResource/Pages/ListPublicationTypes.php
@@ -2,7 +2,7 @@
 
 namespace RDBI\CMS\Filament\Resources\PublicationTypeResource\Pages;
 
-use App\Filament\Resources\PublicationTypeResource;
+use RDBI\CMS\Filament\Resources\PublicationTypeResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
 
