41d65e3d230a7035e9669ee8fed136b11e7faef2
Robert Bardall <robert.bardall@rdbinteractive.com>
2022-12-24 11:43:05 -0500

    Load settings migration

--- src/CMSServiceProvider.php
+++ src/CMSServiceProvider.php
@@ -23,7 +23,10 @@ class CMSServiceProvider extends ServiceProvider
         );
 
         // Migrations
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadMigrationsFrom([
+            __DIR__ . '/database/migrations',
+            __DIR__ . '/database/settings',
+        ]);
 
         // Views
         $this->loadViewsfrom(__DIR__ . '/views', 'rdbi-cms');
@@ -45,30 +48,36 @@ class CMSServiceProvider extends ServiceProvider
          * Publish Required Configuration
          */
         $this->publishes([
-            __DIR__.'/AppServiceProvider.php' => app_path('Providers/AppServiceProvider.php'),
-            __DIR__.'/Models/User.php' => app_path('Models/User.php'),
-            __DIR__.'/config/filament.php' => config_path('filament.php'),
-            __DIR__.'/config/settings.php' => config_path('settings.php'),
-            __DIR__.'/Filament/Pages/Settings.php' => app_path('Filament/Pages/Settings.php'),
-            __DIR__.'/Tests/Feature/ApplicationScaffoldedCorrectlyTest.php' => base_path('tests/Feature/ApplicationScaffoldedCorrectlyTest.php'),
+            __DIR__ . '/AppServiceProvider.php'
+            => app_path('Providers/AppServiceProvider.php'),
+            __DIR__ . '/Models/User.php'
+            => app_path('Models/User.php'),
+            __DIR__ . '/config/filament.php'
+            => config_path('filament.php'),
+            __DIR__ . '/config/settings.php'
+            => config_path('settings.php'),
+            __DIR__ . '/Filament/Pages/Settings.php'
+            => app_path('Filament/Pages/Settings.php'),
+            __DIR__ . '/Tests/Feature/ApplicationScaffoldedCorrectlyTest.php'
+            => base_path('tests/Feature/ApplicationScaffoldedCorrectlyTest.php'),
         ], 'rdbi-config');
 
         /**
          * Publish User Seeder
          */
         $this->publishes([
-            __DIR__.'/Seeders/UserSeeder.php' => database_path('seeders/UserSeeder.php'),
+            __DIR__ . '/Seeders/UserSeeder.php' => database_path('seeders/UserSeeder.php'),
         ], 'rdbi-dev-seeds');
 
         /**
          * Publish script/style build files and configurations.
          */
         $this->publishes([
-                __DIR__ . '/build/filament.css' => resource_path('css/filament.css'),
-                __DIR__ . '/build/package.json' => base_path('package.json'),
-                __DIR__ . '/build/postcss.config.js' => base_path('postcss.config.js'),
-                __DIR__ . '/build/tailwind.config.js' => base_path('tailwind.config.js'),
-                __DIR__ . '/build/webpack.mix.js' => base_path('webpack.mix.js'),
+            __DIR__ . '/build/filament.css' => resource_path('css/filament.css'),
+            __DIR__ . '/build/package.json' => base_path('package.json'),
+            __DIR__ . '/build/postcss.config.js' => base_path('postcss.config.js'),
+            __DIR__ . '/build/tailwind.config.js' => base_path('tailwind.config.js'),
+            __DIR__ . '/build/webpack.mix.js' => base_path('webpack.mix.js'),
         ], 'rdbi-build');
     }
 }
